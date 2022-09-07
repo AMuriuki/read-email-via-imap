881a534bd8b4c87aa60a9a37af63119aac8b0049
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-09-07 16:02:50 +0300

    loop through messages

--- read_email.py
+++ read_email.py
@@ -18,6 +18,35 @@ imap = imaplib.IMAP4_SSL(imap_server)
 # authenticate
 imap.login(username, password)
 
+# select INBOX mailbox
 status, messages = imap.select("INBOX")
 
-print(status, messages)
+# to see available mailboxes
+# imap.list()
+
+# determine number of top emails to fetch
+N = 3
+
+# total number of emails
+messages = int(messages[0])
+
+# loop over each email message
+for i in range(messages, messages-N, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(i), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse the bytes email into an email object
+            msg = email.message_from_bytes(response[1])
+
+            # decode the email subject
+            subject, encoding = decode_header(msg['Subject'])[0]
+
+            if isinstance(subject, bytes):
+                # if bytes decode to str
+                subject = subject.decode(encoding)
+
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+
+            print(subject, From)
